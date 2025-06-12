40d3603b9889f9d8d37bac1654dc62a976c8a302
shinminy <smy11go@gmail.com>
2025-06-12 20:17:08 +0900

    #2 시리즈 생성 기능 추가 : 레디스에 빈 배열 저장할 수 있도록 수정

--- src/main/java/com/posty/postingapi/domain/common/RedisManager.java
+++ src/main/java/com/posty/postingapi/domain/common/RedisManager.java
@@ -1,5 +1,7 @@
 package com.posty.postingapi.domain.common;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
@@ -8,33 +10,49 @@ import java.util.List;
 @Component
 public class RedisManager {
 
+    private static final String EMPTY_PLACEHOLDER = "__EMPTY__";
+
     protected final RedisTemplate<String, String> redisTemplate;
 
     public RedisManager(RedisTemplate<String, String> redisTemplate) {
         this.redisTemplate = redisTemplate;
     }
 
-    public void saveList(String key, List<String> values) {
-        redisTemplate.opsForList().rightPushAll(key, values);
+    public void saveList(@NotEmpty String key, @NotNull List<String> values) {
+        if (values.isEmpty()) {
+            redisTemplate.opsForList().rightPush(key, EMPTY_PLACEHOLDER);
+        } else {
+            redisTemplate.opsForList().rightPushAll(key, values);
+        }
     }
 
-    public List<String> getList(String key) {
-        return redisTemplate.opsForList().range(key, 0, -1);
+    public List<String> getList(@NotEmpty String key) {
+        List<String> list = redisTemplate.opsForList().range(key, 0, -1);
+        if (list != null && list.size() == 1 && EMPTY_PLACEHOLDER.equals(list.get(0))) {
+            return List.of();
+        }
+        return list;
     }
 
-    public void updateList(String key, List<String> values) {
-        redisTemplate.opsForList().rightPushAll(key, values);
+    public void updateList(@NotEmpty String key, @NotNull List<String> values) {
+        redisTemplate.delete(key);
+
+        if (values.isEmpty()) {
+            redisTemplate.opsForList().rightPush(key, EMPTY_PLACEHOLDER);
+        } else {
+            redisTemplate.opsForList().rightPushAll(key, values);
+        }
     }
 
-    public void delete(String key) {
+    public void delete(@NotEmpty String key) {
         redisTemplate.delete(key);
     }
 
-    public boolean hasKey(String key) {
+    public boolean hasKey(@NotEmpty String key) {
         return redisTemplate.hasKey(key);
     }
 
-    public String createKey(String ... keys) {
+    public String createKey(@NotEmpty String ... keys) {
         return String.join(":", keys);
     }
 }
