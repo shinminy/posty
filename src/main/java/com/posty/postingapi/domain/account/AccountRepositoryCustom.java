2fc0f22d4a3d4f1bfd67014a0a1caaa3089be795
shinminy <smy11go@gmail.com>
2025-06-18 20:26:30 +0900

    #65 시리즈의 매니저 계정 status 확인 : 삭제 상태의 계정은 매니저에 넣을 수 없도록 수정

--- src/main/java/com/posty/postingapi/domain/account/AccountRepositoryCustom.java
+++ src/main/java/com/posty/postingapi/domain/account/AccountRepositoryCustom.java
@@ -1,10 +1,12 @@
 package com.posty.postingapi.domain.account;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AccountRepositoryCustom {
     boolean existsNonDeletedByEmail(String email);
     boolean existsNonDeletedByName(String name);
     Optional<Account> findNonDeletedById(Long id);
+    List<Account> findNonDeletedByIdIn(List<Long> ids);
     Optional<Account> findNonDeletedByEmail(String email);
 }
