cc72c8d584488d67071eb0e7175830c266e3395d
shinminy <smy11go@gmail.com>
2025-06-30 15:47:17 +0900

    #68 posting-api 모듈로 분리 : 루트 프로젝트에 남은 이전 파일 제거

--- src/main/java/com/posty/postingapi/domain/post/PostBlockRepositoryCustom.java
+++
@@ -1,8 +0,0 @@
-package com.posty.postingapi.domain.post;
-
-import java.util.List;
-
-public interface PostBlockRepositoryCustom {
-    List<String> findDistinctWriterNamesBySeriesId(Long seriesId);
-    List<String> findDistinctWriterNamesByPostId(Long postId);
-}
