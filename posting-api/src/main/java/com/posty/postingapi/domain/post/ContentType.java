068539d444e88887308635bae34aeecc42cc4440
shinminy <smy11go@gmail.com>
2025-07-23 04:53:19 +0900

    #14 포스트 삭제 기능 추가 : 파일 크기 설명 추가, Swagger UI의 예제에서 MediaContentRequest 예제에 type이 TEXT로 보여 순서 맞추기 위해 변경

--- posting-api/src/main/java/com/posty/postingapi/domain/post/ContentType.java
+++ posting-api/src/main/java/com/posty/postingapi/domain/post/ContentType.java
@@ -4,6 +4,6 @@ package com.posty.postingapi.domain.post;
 // CAUTION: 변경 누락 시 직렬화/역직렬화 오류가 발생할 수 있습니다.
 // LINK: 관련 클래스 - com.posty.postingapi.dto.post.ContentRequest, com.posty.postingapi.dto.post.ContentResponse
 public enum ContentType {
-    TEXT,
     MEDIA,
+    TEXT,
 }
