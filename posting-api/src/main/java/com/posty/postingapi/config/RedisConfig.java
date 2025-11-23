a5064f3574ffd9e7ac974364eb7f849c3b56ad18
shinminy <smy11go@gmail.com>
2025-11-23 22:29:40 +0900

    #104 계정 이름 변경 시 작가 목록 캐시 무효화 : 작가 목록 캐시를 시리즈/포스트별로 작가명(계정명) 목록을 저장하던 방식에서 시리즈/포스트별로 작가ID(계정ID) 목록을 저장하고 계정ID별로 계정명을 따로 캐시화하여 계정 수정 시 계정ID-계정명 캐시만 무효화되도록 수정, 최근에 접근된 계정명 캐시만 살아있또록 계정ID-계정명 캐시에 30일의 유효기간(TTL) 설정, Redis 캐시에 숫자를 저장하는 것에 따른 수정(Redis에서 숫자는 그냥 Integer로 저장되는 것을 읽어올 때 Long으로 읽어올 수 있도록 수정 등), RedisManager::createKey의 매개변수 varargs 배열에는 @NotEmpty가 제대로 동작 안 할 수 있어 수정

--- posting-api/src/main/java/com/posty/postingapi/config/RedisConfig.java
+++ posting-api/src/main/java/com/posty/postingapi/config/RedisConfig.java
@@ -1,10 +1,13 @@
 package com.posty.postingapi.config;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -20,8 +23,19 @@ public class RedisConfig {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
 
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        StringRedisSerializer stringSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringSerializer);
+        redisTemplate.setHashKeySerializer(stringSerializer);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.activateDefaultTyping(
+                objectMapper.getPolymorphicTypeValidator(),
+                ObjectMapper.DefaultTyping.NON_FINAL,
+                JsonTypeInfo.As.PROPERTY
+        );
+        GenericJackson2JsonRedisSerializer jsonSerializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+        redisTemplate.setValueSerializer(jsonSerializer);
+        redisTemplate.setHashValueSerializer(jsonSerializer);
 
         return redisTemplate;
     }
--- posting-api/src/main/java/com/posty/postingapi/infrastructure/cache/WriterCacheManager.java
+++ posting-api/src/main/java/com/posty/postingapi/infrastructure/cache/WriterCacheManager.java
@@ -1,60 +1,145 @@
 package com.posty.postingapi.infrastructure.cache;
 
+import com.posty.postingapi.domain.account.Account;
+import com.posty.postingapi.domain.account.AccountRepository;
 import com.posty.postingapi.domain.post.PostBlockRepository;
+import com.posty.postingapi.properties.TimeToLiveConfig;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import java.time.Duration;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Component
 public class WriterCacheManager {
 
-    private static final String WRITER_KEY = "writers";
+    private static final String WRITER_IDS_KEY = "writerIds";
+    private static final String NAME_KEY = "name";
 
     private final RedisManager redisManager;
+    private final AccountRepository accountRepository;
     private final PostBlockRepository postBlockRepository;
 
-    public WriterCacheManager(RedisManager redisManager, PostBlockRepository postBlockRepository) {
+    private final Duration accountNameTtl;
+
+    public WriterCacheManager(RedisManager redisManager,
+                              AccountRepository accountRepository,
+                              PostBlockRepository postBlockRepository,
+                              TimeToLiveConfig timeToLiveConfig) {
         this.redisManager = redisManager;
+        this.accountRepository = accountRepository;
         this.postBlockRepository = postBlockRepository;
+
+        this.accountNameTtl = timeToLiveConfig.getAccountNameCache();
+    }
+
+    private String createSeriesWriterIdsKey(long seriesId) {
+        return redisManager.createKey("series", String.valueOf(seriesId), WRITER_IDS_KEY);
+    }
+
+    private String createPostWriterIdsKey(long postId) {
+        return redisManager.createKey("post", String.valueOf(postId), WRITER_IDS_KEY);
+    }
+
+    private String createAccountNameKey(long accountId) {
+        return redisManager.createKey("account", String.valueOf(accountId), NAME_KEY);
+    }
+
+    private List<String> loadAccountNames(List<Long> accountIds) {
+        Map<Long, String> idToKey = accountIds.stream()
+                .collect(Collectors.toMap(
+                        Function.identity(),
+                        this::createAccountNameKey
+                ));
+
+        Map<String, Object> cachedNameMap = redisManager.getValuesAsMap(new ArrayList<>(idToKey.values()));
+
+        List<String> names = new ArrayList<>(accountIds.size());
+
+        List<Long> missingIds = new ArrayList<>();
+
+        for (Long accountId : accountIds) {
+            String key = idToKey.get(accountId);
+            Object value = cachedNameMap.get(key);
+
+            if (value instanceof String name) {
+                names.add(name);
+            } else {
+                missingIds.add(accountId);
+            }
+        }
+
+        if (!missingIds.isEmpty()) {
+            List<Account> accounts = accountRepository.findAllById(missingIds);
+
+            Map<String, Object> missingNameMap = new HashMap<>();
+
+            for (Account account : accounts) {
+                String name = account.getName();
+
+                names.add(name);
+                missingNameMap.put(createAccountNameKey(account.getId()), name);
+            }
+
+            if (!missingNameMap.isEmpty()) {
+                redisManager.saveValuesWithTtl(missingNameMap, accountNameTtl);
+            }
+        }
+
+        Collections.sort(names);
+
+        return names;
     }
 
     public List<String> loadWritersOfSeries(long seriesId) {
-        String redisKey = redisManager.createKey("series", String.valueOf(seriesId), WRITER_KEY);
+        List<Long> writerIds = loadWriterIdsOfSeries(seriesId);
+        return loadAccountNames(writerIds);
+    }
 
-        List<String> writers;
+    private List<Long> loadWriterIdsOfSeries(long seriesId) {
+        String redisKey = createSeriesWriterIdsKey(seriesId);
+
+        List<Long> writerIds;
         if (redisManager.hasKey(redisKey)) {
-            writers = redisManager.getList(redisKey);
+            writerIds = redisManager.getList(redisKey, Long.class);
         } else {
-            writers = postBlockRepository.findDistinctWriterNamesBySeriesId(seriesId);
-            redisManager.saveList(redisKey, writers);
+            writerIds = postBlockRepository.findDistinctWriterIdsBySeriesId(seriesId);
+            redisManager.saveList(redisKey, writerIds);
         }
 
-        return writers;
+        return writerIds;
     }
 
     public List<String> loadWritersOfPosts(long postId) {
-        String redisKey = redisManager.createKey("post", String.valueOf(postId), WRITER_KEY);
+        List<Long> writerIds = loadWriterIdsOfPosts(postId);
+        return loadAccountNames(writerIds);
+    }
 
-        List<String> writers;
+    private List<Long> loadWriterIdsOfPosts(long postId) {
+        String redisKey = createPostWriterIdsKey(postId);
+
+        List<Long> writerIds;
         if (redisManager.hasKey(redisKey)) {
-            writers = redisManager.getList(redisKey);
+            writerIds = redisManager.getList(redisKey, Long.class);
         } else {
-            writers = postBlockRepository.findDistinctWriterNamesByPostId(postId);
-            redisManager.saveList(redisKey, writers);
+            writerIds = postBlockRepository.findDistinctWriterIdsByPostId(postId);
+            redisManager.saveList(redisKey, writerIds);
         }
 
-        return writers;
+        return writerIds;
     }
 
     public void clearWritersOfSeries(long seriesId) {
-        String redisKey = redisManager.createKey("series", String.valueOf(seriesId), WRITER_KEY);
-        redisManager.delete(redisKey);
+        redisManager.delete(createSeriesWriterIdsKey(seriesId));
     }
 
     public void clearWritersOfPosts(long postId, long seriesId) {
-        String redisKey = redisManager.createKey("post", String.valueOf(postId), WRITER_KEY);
-        redisManager.delete(redisKey);
-
+        redisManager.delete(createPostWriterIdsKey(postId));
         clearWritersOfSeries(seriesId);
+    }
+
+    public void clearAccountName(long accountId) {
+        redisManager.delete(createAccountNameKey(accountId));
     }
 }
