f5d4295513a3ed7d2743a67339d03c00c33cdf56
shinminy <smy11go@gmail.com>
2026-01-04 11:49:51 +0900

    chore(test): 확인 및 적용 완료된 쿼리 제거 (#132)

--- sql/dml/check_data.sql
+++ sql/dml/check_data.sql
@@ -7,10 +7,3 @@ select * from post;
 select * from post_block;
 select * from comment;
 select now();
-
-SHOW INDEX FROM account; -- Key_name이 인덱스명
-ALTER TABLE account DROP INDEX email;
-ALTER TABLE account DROP INDEX name;
-
-select * from media;
-update media set stored_url = REPLACE(stored_url, '/file/', '/') where stored_url like '%/file/%';
