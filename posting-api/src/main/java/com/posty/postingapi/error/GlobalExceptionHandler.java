5ab4604aaf86a702e67808539490457028c5f33c
shinminy <smy11go@gmail.com>
2026-01-09 06:58:02 +0900

    chore(error): 미사용 import 제거 (#135)

--- posting-api/src/main/java/com/posty/postingapi/error/GlobalExceptionHandler.java
+++ posting-api/src/main/java/com/posty/postingapi/error/GlobalExceptionHandler.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
